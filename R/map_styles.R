5b6fd1b1569b8834f1e9b2fdd5457df49874e4bc
Anthony North <anthony.jl.north@gmail.com>
2020-07-02 18:07:57 +1000

    add mapbox style constants #1

---
+++ R/map_styles.R
@@ -0,0 +1,30 @@
+#' Map styles
+#'
+#' @name map_styles
+#' @rdname map_styles
+#' @seealso  <https://docs.mapbox.com/api/maps/#mapbox-styles>
+NULL
+
+#' @rdname map_styles
+#' @export
+mapbox_streets <- "mapbox://styles/mapbox/streets-v11"
+
+#' @rdname map_styles
+#' @export
+mapbox_outdoors <- "mapbox://styles/mapbox/outdoors-v11"
+
+#' @rdname map_styles
+#' @export
+mapbox_light <- "mapbox://styles/mapbox/light-v10"
+
+#' @rdname map_styles
+#' @export
+mapbox_dark <- "mapbox://styles/mapbox/dark-v10"
+
+#' @rdname map_styles
+#' @export
+mapbox_satellite <- "mapbox://styles/mapbox/satellite-v9"
+
+#' @rdname map_styles
+#' @export
+mapbox_satellite_streets <- "mapbox://styles/mapbox/satellite-streets-v11"
