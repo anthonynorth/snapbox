07a7d784fb2764f65466d433e729c0c877d031e0
Anthony North <anthony.jl.north@gmail.com>
2020-08-12 19:13:17 +1000

    fix mercator bbox test

--- tests/testthat/test-area.R
+++ tests/testthat/test-area.R
@@ -1,7 +1,9 @@
 test_that("mercator bbox calculation works", {
 
-  brisbane_point <- sf::st_sf(sf::st_sfc(sf::st_point(c(153.03792, -27.414733))),
-                          crs = 4326)
+  brisbane_point <- sf::st_sf(
+    sf::st_sfc(sf::st_point(c(153.03792, -27.414733))),
+    crs = 4326
+  )
 
   expect_error(get_mercator_bbox(brisbane_point),
                "Bounding box of supplied geometry has 0 area")
@@ -10,9 +12,13 @@ test_that("mercator bbox calculation works", {
                "Bounding box of supplied geometry has 0 area")
 
   brisbane_bbox <- get_mercator_bbox(brisbane_point, area_buffer = 50)
-  expect_equal(brisbane_bbox$xmax - brisbane_bbox$xmin,
-               100)
-  expect_equal(brisbane_bbox$ymax - brisbane_bbox$ymin,
-               100)
+  expect_equal(
+    as.numeric(brisbane_bbox$xmax - brisbane_bbox$xmin),
+    100
+  )
+  expect_equal(
+    as.numeric(brisbane_bbox$ymax - brisbane_bbox$ymin),
+    100
+  )
 
 })
