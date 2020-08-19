fe6edf69015d7eef7e49bf7f7f2b42617a370d55
Miles McBain <miles.mcbain@qfes.qld.edu.au>
2020-08-19 13:47:58 +1000

    work with point geometry with many points
    fixes #8

--- R/sf.R
+++ R/sf.R
@@ -12,15 +12,29 @@ get_mercator_bbox <- function(area, area_buffer = 0) {
 }
 
 get_mercator_bbox.sfc <- function(area, area_buffer = 0) {
-  buffered_area <- area %>%
-    sf::st_transform(3857) %>%
-    sf::st_buffer(area_buffer)
 
-  if (as.numeric(sf::st_area(buffered_area)) == 0) {
+  area_web_mercator <-
+    sf::st_transform(area, 3857)
+
+  if (area_buffer > 0) {
+    target_area <-
+      area_web_mercator %>%
+      sf::st_buffer(area_buffer)
+  } else {
+    target_area <-
+      area_web_mercator
+  }
+
+  area_bbox_mercator <- sf::st_bbox(target_area)
+  area_bbox_mercator_area <-
+    (area_bbox_mercator$xmax - area_bbox_mercator$xmin) *
+    (area_bbox_mercator$ymax - area_bbox_mercator$ymin)
+
+  if (is_zero(area_bbox_mercator_area)) {
     stop("Bounding box of supplied geometry has 0 area, so cannot be used as a map viewport. Try setting 'buffer' argument.")
   }
 
-  sf::st_bbox(buffered_area)
+  area_bbox_mercator
 }
 
 get_mercator_bbox.sf <- function(area, area_buffer = 0) {
@@ -43,3 +57,14 @@ get_aspect_ratio <- function(bbox) {
 
   as.numeric(abs(bbox$xmax - bbox$xmin) / abs(bbox$ymax - bbox$ymin))
 }
+##' is_zero
+##'
+##' using all.equal to allow for areas that are very very small to be considered
+##' as equal to zero.
+##'
+##' @name is_zero
+##' @param x the number to compare to 0
+##' @return {boolean}
+##'
+##' @nord
+is_zero <- function(x) isTRUE(all.equal(0, unname(x)))
--- tests/testthat/test-area.R
+++ tests/testthat/test-area.R
@@ -5,9 +5,9 @@ test_that("mercator bbox calculation works", {
     crs = 4326
   )
 
+  ## single points should fail
   expect_error(get_mercator_bbox(brisbane_point),
                "Bounding box of supplied geometry has 0 area")
-
   expect_error(get_static_map(brisbane_point),
                "Bounding box of supplied geometry has 0 area")
 
@@ -20,5 +20,17 @@ test_that("mercator bbox calculation works", {
     as.numeric(brisbane_bbox$ymax - brisbane_bbox$ymin),
     100
   )
+
+  ## many points should work
+  brisbane_2point <- sf::st_sf(
+                           sf::st_sfc(list(sf::st_point(c(153.03792, -27.414733)),
+                                      sf::st_point(c(153.035, -27.415)))),
+                          crs = 4326
+                        )
+  expect_equal(
+    as.numeric(get_mercator_bbox(brisbane_2point)),
+    c(17035778.2735486, -3175416.88179225, 17036103.3264617, -3175383.39926467)
+  )
+ 
 
 })
